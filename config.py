08f89fec06bebd5e39889777cb1460a08b22c78d
loveppb12-cmyk <loveppb12@gmail.com>
2026-03-11 00:10:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 # Bot Configuration
 API_ID = 20284828
 API_HASH = "a980ba25306901d5c9b899414d6a9ab7"
-BOT_TOKEN = "8796521549:AAHzmD5tDVbnVFlGolYp4IeBbxeQ2Bb9qrg"
-SESSION_STRING = "1BJWap1sBuzt45NPkbRzSyQoKzlgYsRfG7-hkA9GIgh1RW050PZB_Xrf_rklSV45kk0JjuLtiG9ztcq3-nSfs9q_RzYZjyn484zh1vgvpxn_iNHxvLszeqnleSZAt9pLmf50aMDe-mK_bS41hLWCgPkgx2BeGzShaxCX4QS8YcZrowqbTZllaMQzOjAA6WmkIsTB7GpnkrGUjphM7cCV1QrJ1qZeUZoQn7HhCKw0AeuK-vzoNac_A6HkaYBpxFdcrbW-B0DymtkPrrm_OCB17TWkrXgZKteGi46rxHGafeKuwcQvCYJ1efZHhg8R3isZF-oTuHVRIdGQAiz1Ri77JZdQRQ6Hq-sA="
+BOT_TOKEN = "qrg"
+SESSION_STRING = "77JZdQRQ6Hq-sA="
 
 # Deletion delay in seconds
 DELETE_DELAY = 180
