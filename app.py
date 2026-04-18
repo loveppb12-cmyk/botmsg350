56b809907b56a2d4cc1f1eaae368a77e62bc30ec
loveppb12-cmyk <loveppb12@gmail.com>
2026-04-18 19:52:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # Configuration
-DELETE_DELAY = 189 # 210 seconds
+DELETE_DELAY = 10 # 210 seconds
 
 class TelegramMessageDeleter:
     def __init__(self):
